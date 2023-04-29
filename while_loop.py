7a1c52ca9d4093a71e18f0f9a252c3ed611ab9dc
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2023-04-29 12:38:31 +0530

    While Loop

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+#The loop continues to execute the code within it until the condition becomes false.
+#Program 
+
+i = 1
+while True:
+    if i%3 == 0:
+        break
+    print(i)
+    i += 1
+    
+    #Most Basic Fundamental Loop in all languages..
